943efc4603de1b21f5509077f34bfdf917f3b505
DucThanh <phoducthanh@gmail.com>
2024-10-21 22:19:42 +0700

    FE and BE

---
+++ be-web-food-order/Services/Auth/WFO.Auth.ApplicationService/Startup/AuthStartUp.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFO.Auth.ApplicationService.UserModule.Abstracts;
+using WFO.Auth.ApplicationService.UserModule.Implements;
+using WFO.Auth.Infrastructures;
+using WFO.Shared.Constant.Database;
+
+namespace WFO.Auth.ApplicationService.Startup
+{
+    public static class AuthStartup
+    {
+        /// <summary>
+        /// extension method to configure the auth database
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="assemblyName"></param>
+        public static void ConfigureAuth(this WebApplicationBuilder builder, string? assemblyName)
+        {
+            builder.Services.AddDbContext<AuthDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(
+                        builder.Configuration.GetConnectionString("Default"),
+                        options =>
+                        {
+                            options.MigrationsAssembly(assemblyName);
+                            options.MigrationsHistoryTable(
+                                DbSchema.TableMigrationsHistory,
+                                DbSchema.Auth
+                            );
+                        }
+                    );
+                },
+                ServiceLifetime.Scoped
+            );
+
+            builder.Services.AddScoped<IAuthService, AuthService>();
+        }
+    }
+}
---
+++ be-web-food-order/Services/Shared/WFO.Shared.Constant/Database/DbSchema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFO.Shared.Constant.Database
+{
+    public static class DbSchema
+    {
+        public const string TableMigrationsHistory = "__EFMigrationsHistory";
+        public const string Default = "dbo";
+        public const string Auth = "auth";
+        public const string Product = "prod";
+        public const string Order = "ord";
+    }
+}
